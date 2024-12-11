56cd9845096e30c57e8d94bc7df7ebb75a5e1b82
riddsep <ridoseptiawan00@gmail.com>
2024-12-11 22:30:16 +0700

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -197,12 +197,12 @@ function Total() {
     </div>
   );
 }
-// function Button({ children, className, onClick }) {
-//   return (
-//     <button className={`px-4 py-2 ${className}`} onClick={onClick}>
-//       {children}
-//     </button>
-//   );
-// }
+function Button({ children, className, onClick }) {
+  return (
+    <button className={`px-4 py-2 ${className}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
 
 export default App;
