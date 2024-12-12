fee4852943d8b9df8d879d44b880f18a8504c535
riddsep <ridoseptiawan00@gmail.com>
2024-12-12 19:09:49 +0700

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -296,19 +296,19 @@ function CartProduct({ addToCart, setShowModal }) {
   );
 }
 
-// function Total({ addToCart }) {
-//   return (
-//     <div className="flex justify-between items-center py-2 mb-5">
-//       <h1 className="text-xl font-semibold">Total</h1>
-//       <span className="mr-2  font-semibold text-[#C73A0F] text-xl">
-//         $
-//         {addToCart
-//           .reduce((acc, curr) => acc + (curr.quantity * curr.price) / 100, 0)
-//           .toFixed(2)}
-//       </span>
-//     </div>
-//   );
-// }
+function Total({ addToCart }) {
+  return (
+    <div className="flex justify-between items-center py-2 mb-5">
+      <h1 className="text-xl font-semibold">Total</h1>
+      <span className="mr-2  font-semibold text-[#C73A0F] text-xl">
+        $
+        {addToCart
+          .reduce((acc, curr) => acc + (curr.quantity * curr.price) / 100, 0)
+          .toFixed(2)}
+      </span>
+    </div>
+  );
+}
 
 function Modal({ setShowModal, addToCart, onOrder }) {
   return (
